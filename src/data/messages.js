3b28561bab791c657e2ade5da1b632a8eb19efcf
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2025-11-10 13:30:39 -0500

    more minor css and message array tweaks

--- src/data/messages.js
+++ src/data/messages.js
@@ -10,30 +10,22 @@ const messages = [
   },
   {
     id: 1,
-    user: "Amandaconda",
-    added: "Mon - Nov 03, 2025, 3:41:11 PM EST",
-    text: "Effing fun!!! 😀",
-    likes: 3,
+    user: "James Paul",
+    added: "Tue - Nov 04, 2025, 8:12:01 AM EST",
+    text: "Speaking of swearing a lot, don't forget to vote today.🎃",
+    likes: 2,
     likedBy: [], // This tracks which users have liked this posting
   },
   {
     id: 2,
-    user: "James Paul",
-    added: "Tue - Nov 04, 2025, 8:12:01 AM EST",
-    text: "Speaking of swearing a lot, don't forget to vote today.🎃",
+    user: "Montague The Dog 🐶",
+    added: "Tue - Nov 04, 2025, 12:11:36 PM EST",
+    text: "Roses are red, violets are blue, I'm hungry for snackies, why aren't you? 🍗🥓🧀🍕🍖",
     likes: 3,
     likedBy: [], // This tracks which users have liked this posting
   },
   {
     id: 3,
-    user: "Montague The Dog 🐶",
-    added: "Tue - Nov 04, 2025, 12:11:36 PM EST",
-    text: "Roses are red, violets are blue, I'm hungry for snackies, why aren't you? 🍗🥓🧀🍕🍖",
-    likes: 5,
-    likedBy: [], // This tracks which users have liked this posting
-  },
-  {
-    id: 4,
     user: "Odd-fact Jack",
     added: "Tue - Nov 04, 2025, 5:45:54 PM EST",
     text: "The longest place name in the English-speaking world is Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu in New Zealand, which has 85 letters.",
